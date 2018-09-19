013a23f28f65519bfa7469fd697170fdc7ac650c
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2018-09-19 12:03:25 -0400

    mostrar informacion del usuario

--- src/app/usuarios/usuario/usuario.component.ts
+++ src/app/usuarios/usuario/usuario.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AppState } from '../../store/app.reducers';
 import { Store } from '@ngrx/store';
 import { CargarUsuario } from '../../store/actions/usuario.actions';
+import { Usuario } from '../../models/usuario.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html',
   styles: []
 })
-export class UsuarioComponent implements OnInit {
+export class UsuarioComponent implements OnInit, OnDestroy {
 
+  usuario: Usuario;
+  subs: Subscription = new Subscription();
+  loading: boolean;
+  error: any;
   constructor(
     private router: ActivatedRoute,
     private store: Store<AppState>
@@ -25,6 +31,16 @@ export class UsuarioComponent implements OnInit {
 
       }
     );
+    this.subs = this.store.select('usuario').subscribe(
+      user => {
+        this.error = user.error;
+        this.loading = user.loading;
+        this.usuario = user.user;
+      }
+    );
+  }
+  ngOnDestroy() {
+    this.subs.unsubscribe();
   }
 
 }
